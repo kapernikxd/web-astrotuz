f1e7bfcb206906e8c4da1874ede5f81ab90e3124
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-26 18:29:35 +0100

    Move zodiac data to config

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,4 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'pages.natal')->name('natal');
+Route::get('/', function () {
+    $zodiac = config('zodiac');
+    $active = $zodiac['oven'];
+
+    return view('pages.natal', [
+        'activeKey' => $active['key'],
+        'activeLabel' => $active['label'],
+        'activeDescription' => $active['description'],
+    ]);
+})->name('natal');
+
+Route::get('{sign}/harakteristika', function (string $sign) {
+    $zodiac = config('zodiac');
+
+    if (!array_key_exists($sign, $zodiac)) {
+        abort(404);
+    }
+
+    $active = $zodiac[$sign];
+
+    return view('pages.natal', [
+        'activeKey' => $active['key'],
+        'activeLabel' => $active['label'],
+        'activeDescription' => $active['description'],
+    ]);
+})->name('zodiac.show');
