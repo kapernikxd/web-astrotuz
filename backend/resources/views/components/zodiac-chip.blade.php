3576cf078fe76719a5c4483fde50fe65bf54fecf
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-12-27 13:31:37 +0100

    Add admin-managed right sidebar blocks

--- backend/resources/views/components/zodiac-chip.blade.php
+++ backend/resources/views/components/zodiac-chip.blade.php
@@ -1,11 +1,21 @@
 @props([
   'label',
   'active' => false,
+  'url' => null,
 ])
 
-<button {{ $attributes->merge(['class' => 'zodiac-chip ' . ($active ? 'is-active' : '')]) }} type="button">
-    <span class="zodiac-icon">
-        {{ $slot }}
-    </span>
-    <span class="zodiac-label">{{ $label }}</span>
-</button>
+@if ($url)
+    <a {{ $attributes->merge(['class' => 'zodiac-chip ' . ($active ? 'is-active' : '')]) }} href="{{ $url }}">
+        <span class="zodiac-icon">
+            {{ $slot }}
+        </span>
+        <span class="zodiac-label">{{ $label }}</span>
+    </a>
+@else
+    <button {{ $attributes->merge(['class' => 'zodiac-chip ' . ($active ? 'is-active' : '')]) }} type="button">
+        <span class="zodiac-icon">
+            {{ $slot }}
+        </span>
+        <span class="zodiac-label">{{ $label }}</span>
+    </button>
+@endif
